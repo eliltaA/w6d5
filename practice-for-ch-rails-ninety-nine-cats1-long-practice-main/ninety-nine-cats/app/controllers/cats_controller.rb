5df279963da3c1c8b59e41eff5852fd605e1ef16
eliltaA <eliltatabrham@gmail.com>
2023-10-07 10:39:04 -0700

    add

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/controllers/cats_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -5,13 +5,24 @@ class CatsController < ApplicationController
     end
 
     def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+           redirect_to cat_url(@cat)
+        else
+            flash.now[:errors] = @cat.errors.full_messages
+            render :new
+        end
     end
 
     def new
+        @cat = Cat.new
+      render :new
     end
 
     def edit
-    end
+        @cat = Cat.find(params[:id])
+        render :edit
+    end 
 
     def show
         @cat = Cat.find(params[:id])
@@ -19,6 +30,13 @@ class CatsController < ApplicationController
     end
 
     def update
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            flash.now[:errors] = @cat.errors.full_messages
+            render :edit
+        end
     end
 
     private
--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
@@ -1,14 +1,15 @@
 class Cat < ApplicationRecord
-    cat_color = ['white', 'black', 'brown']
+  
+    CAT_COLOR = ['white', 'black', 'brown']
     validates :birth_date, :color, :name, :sex, presence: true
-    validates :color, inclusion:{in:cat_color}
+    validates :color, inclusion:{in:CAT_COLOR}
     validates :sex, inclusion:{in:['M', 'F']}
     validate :birth_date_cannot_be_future
 # validates color, inclusion: cat_color
 
     def birth_date_cannot_be_future
-       if birth_date > Date.today
-        errors.add(:birth_date, "can't be in the past")
+       if birth_date.present? && birth_date > Date.today
+        errors.add(:birth_date, "can't be in the future")
       end
     end
 end
